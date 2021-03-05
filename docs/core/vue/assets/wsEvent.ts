05913571cc58e46feb7afdaa19510bcc66921165
huangjianbin <hjb@emspost.com.cn>
2021-03-05 14:22:36 +0800

    feat:update

---
+++ docs/core/vue/assets/wsEvent.ts
@@ -0,0 +1,83 @@
+interface wsResult<T = any> {
+  msgType: string;
+  msg: string;
+  data: T;
+}
+
+export class WsEvent {
+  // 单例
+  constructor() {
+    if (!this.instance) this.instance = new WsEvent();
+    return this.instance;
+  }
+
+  private instance!: WsEvent;
+
+  /**
+   * 事件回调队列 多个时会循环执行
+   */
+  private eventQueueMap: {
+    [k: string]: Array<{
+      // 额外参数
+      param: any;
+      // 事件
+      event: (data: wsResult, ...args: any) => void;
+    }>;
+  } = {};
+
+  /**
+   * 事件队列添加事件
+   * @param key 事件名称
+   * @param event 事件
+   * @param param 额外参数
+   */
+  $on(key: string, event: (data: wsResult, ...args: any) => void, param?: any) {
+    if (!this.eventQueueMap[key]) this.eventQueueMap[key] = [];
+
+    this.eventQueueMap[key].push({
+      event: event,
+      param: param,
+    });
+  }
+
+  /**
+   * 从事件队列中删除事件
+   * @param key 事件名称
+   * @param event 事件
+   */
+  $off(key: string, event: Function) {
+    const eventQueue = this.eventQueueMap[key];
+
+    if (eventQueue) {
+      for (let i = eventQueue.length - 1; i >= 0; i--) {
+        if (eventQueue[i].event === event) {
+          eventQueue.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  /**
+   * 执行事件队列
+   */
+  $exec(evt: MessageEvent) {
+    try {
+      const result: wsResult = JSON.parse(evt.data);
+      const { msgType } = result;
+      const eventQueueKeys = Object.keys(this.eventQueueMap).filter(
+        (key) => key === msgType
+      );
+      // 执行指定类型事件
+      eventQueueKeys.forEach((key) => {
+        const eventMap = this.eventQueueMap[key];
+        eventMap.forEach((events) => {
+          events.event(result, events.param);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default new WsEvent();
