062a1f216cfc1207cfff2a833bcf945c46778978
huangjianbin <347468939@qq.com>
2023-02-18 18:09:57 +0800

    feat: update docs

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -10,10 +10,16 @@ const commitRE =
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-      chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
       `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-      `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+      `    ${chalk.green(
+        `fix(v-model): handle events on blur (close #28)`
+      )}\n\n` +
       chalk.red(`  See .github/commit-convention.md for more details.\n`)
   )
   process.exit(1)
